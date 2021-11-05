cf11ef1071954efb23615cc6a9dcdcce87aacb2e
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-11-05 11:42:03 -0600

    Migrated design to handlebars layout, as well, the views are divided by 2 systems, te system for clients and the workers system

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ app.engine(".hbs",
   exphbs({
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
-    partialsDir: path.join(app.get("views"), "partials"),
+    partialsDir: path.join(app.get("views"), "components"),
     extname: ".hbs",
   })
 );
 app.set("view engine", ".hbs");
+
+//static files (css, html, js, media resources)
+app.use(express.static(path.join(__dirname,"public")))
 
 //Middlewares
 app.use(morgan("dev"));
@@ -25,9 +28,47 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.get('/',(req, res)=>{
-    res.render('index')
+//all system render views
+app.get('/system',(req, res)=>{
+  res.render('system/auth/login')
 })
+app.get('/admin/dashboard',(req, res)=>{
+    res.render('system/admin/dashboard')
+})
+app.get('/chef/dashboard',(req, res)=>{
+  res.render('chef/dashboard')
+})
+app.get('/barman/dashboard',(req, res)=>{
+  res.render('system/barman/dashboard')
+})
+app.get('/mesero/dashboard',(req, res)=>{
+  res.render('system/mesero/dashboard')
+})
+app.get('/repartidor/dashboard',(req, res)=>{
+  res.render('system/repartidor/dashboard')
+})
+
+
+//al client render views
+app.get('/login',(req, res)=>{
+  res.render('client/auth/login')
+})
+app.get('/signup',(req, res)=>{
+  res.render('client/auth/signup')
+})
+
+app.get('/client/profile',(req, res)=>{
+  res.render('client/profile')
+})
+app.get('/client/dashboard',(req, res)=>{
+  res.render('client/dashboard')
+})
+
+//lading page (initial page)
+app.get('/',(req, res)=>{
+  res.render('index')
+})
+
 
 //Server port config
 let port = 3000 || process.env.PORT
