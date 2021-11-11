e636556a0448953f7ad109fb832996121a53cfb5
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-11-11 00:46:31 -0600

    Casi se termina el modulo de ordenes para el mesero, falta enviar al chef / barman y falta enviar a cobro

--- src/controllers/systemControllers/Api/apiController.js
+++ src/controllers/systemControllers/Api/apiController.js
@@ -5,10 +5,16 @@ const connection = require('../../../database')
 
 controller.getOrderDetail = async(req, res)=>{
     const {id} = req.params;
-    const orderProducts = await connection.query(`SELECT ppl.fk_pedidolocal, p.nombre,ppl.cantidad FROM productospedidolocal ppl, productos p 
+    const orderProducts = await connection.query(`SELECT ppl.num, ppl.fk_pedidolocal, p.nombre,p.precio_venta,ppl.cantidad, (p.precio_venta*ppl.cantidad) AS total FROM productospedidolocal ppl, productos p 
     WHERE ppl.fk_pedidolocal = ${id} && ppl.fk_producto = p.id`)
     res.json(orderProducts)
 }
+
+controller.getProducts = async(req, res)=>{
+    const products = await connection.query(`select * from productos`)
+    res.json(products)
+}
+
 
 
 module.exports = controller;
--- src/routes/systemRoutes/waiter.routes.js
+++ src/routes/systemRoutes/waiter.routes.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 const waiterControllersPath = '../../controllers/systemControllers/Waiter/'
 
-
 const systemWaiterController = require(waiterControllersPath+'waiter.controller')
 const systemOrdersController = require(waiterControllersPath+'orders.controller')
 
 
 router.get('/waiter/dashboard',systemWaiterController.renderWaiterDashboard)
 router.get('/waiter/orders', systemOrdersController.renderOrdersView)
+
+router.post('/waiter/neworder',systemOrdersController.newOrder)
+
+router.post('/waiter/order/addproduct/:fk_pedidolocal',systemOrdersController.orderNewProduct)
+
+router.get('/waiter/orders/deleteproduct/:num',systemOrdersController.deleteOrderProduct)
 
 
 
