b4d0a79cdae3748d848a5dd69ef9941681c6cb6f
dianastaystoned <dtr.3838@gmail.com>
2021-11-19 00:21:44 -0600

    Se corrigieron errores

--- src/controllers/landinPage.controller.js
+++ src/controllers/landinPage.controller.js
@@ -2,10 +2,9 @@ const connection = require('../database')
 const controller = {}
 
 controller.renderLanding = async (req, res)=>{
-    const categoria = await connection.query('select * from categoria')
     const product = await connection.query('select * from productos limit 6')
-    res.render('index.hbs',{
-       categoria, product
+    res.render('index.hbs',{ 
+       product
     })
   }
 
