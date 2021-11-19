7959e4ffb9001daa3b6ac5a96ccbc28ad84ee6fb
dianastaystoned <dtr.3838@gmail.com>
2021-11-19 00:16:22 -0600

    Se corrigieron errores varios

--- src/controllers/landinPage.controller.js
+++ src/controllers/landinPage.controller.js
@@ -3,7 +3,7 @@ const controller = {}
 
 controller.renderLanding = async (req, res)=>{
     const categoria = await connection.query('select * from categoria')
-    const product = await connection.query('select * from productos')
+    const product = await connection.query('select * from productos limit 6')
     res.render('index.hbs',{
        categoria, product
     })
