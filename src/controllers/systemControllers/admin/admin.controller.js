99f7a5ec7bd92c6952286ffceacee3d1be2c8269
Juan Luis <jl.noriega2000@gmail.com>
2021-11-22 16:16:37 -0600

    Se agrego el funcionamiento de restar los productos del almacen y se empezo la programacion de los dashboard

--- src/controllers/systemControllers/admin/admin.controller.js
+++ src/controllers/systemControllers/admin/admin.controller.js
@@ -4,18 +4,29 @@ const connection = require('../../../database')
 
  controller.renderAdminDashboard = async(req, res)=>{
    //getting all sales
-   const GetLocalSales  = await connection.query('select COUNT (*) as localsales from ventalocal')
-   const localSales = GetLocalSales[0].localsales
-   const GetOnlineSales  = await connection.query('select COUNT (*) as onlinesales from ventalinea')
-   const onlineSales = GetOnlineSales[0].onlinesales
-   const totalSales = localSales + onlineSales;
+   let today = new Date()
+   const fecha = today.toLocaleDateString("en-US")
+   const GetLocalSalesToday  = await connection.query('select COUNT (*) as localsales from ventalocal where fecha = ?', [fecha])
+   const localSalesToday = GetLocalSalesToday[0].localsales
+   const GetOnlineSalesToday = await connection.query('select COUNT (*) as onlinesales from ventalinea where fecha = ?', [fecha]) 
+   const onlineSalesToday = GetOnlineSalesToday[0].onlinesales
+   const GetLocalSalesTotal = await connection.query('select COUNT (*) as localsales from ventalocal')
+   const localSalesTotal = GetLocalSalesTotal[0].localsales
+   const GetOnlineSalesTotal  = await connection.query('select COUNT (*) as onlinesales from ventalinea')
+   const onlineSalesTotal = GetOnlineSalesTotal[0].onlinesales
+   const totalSales = parseInt(localSalesTotal) + parseInt(onlineSalesTotal)
+   console.log(localSalesToday)
    //getting customers
-  //  const GetCustomers = await connection.query('select COUNT (*) as customers from clientes')
-   const GetEmployees = await connection.query('select COUNT (*) as employees from usuarios')
+   const GetCustomers = await connection.query('select COUNT (*) as customers from usuarios where fk_rol = 6')
+   const GetAdmins = await connection.query('select COUNT (*) as admins from usuarios where fk_rol = 1')
+   const GetCocineros = await connection.query('select COUNT (*) as cocineros from usuarios where fk_rol = 2')
+   const GetBartenders = await connection.query('select COUNT (*) as bartenders from usuarios where fk_rol = 3')
+   const GetMeseros = await connection.query('select COUNT (*) as meseros from usuarios where fk_rol = 4')
+   const GetRepartidores = await connection.query('select COUNT (*) as repartidores from usuarios where fk_rol = 5')
     res.render('system/admin/dashboard',{
-      localSales, onlineSales, totalSales, 
-      // Customers: GetCustomers[0].customers,
-      Employees: GetEmployees[0].employees
+      localSalesToday, onlineSalesToday, localSalesTotal, onlineSalesTotal, totalSales, 
+      Customers: GetCustomers[0].customers, Admins: GetAdmins[0].admins, Cocineros: GetCocineros[0].cocineros,
+      Bartenders: GetBartenders[0].bartenders, Meseros: GetMeseros[0].meseros, Repartidores: GetRepartidores[0].repartidores       
     }) 
  }
 
--- src/controllers/systemControllers/admin/payment.controller.js
+++ src/controllers/systemControllers/admin/payment.controller.js
@@ -4,7 +4,7 @@ const controller = {}
 const redirectPath = '/admin/payment'
 
 controller.renderPayment = async (req, res) => {
-    const payment = await connection.query('select p.id, m.mesa, p.totalpedido from pedidolocal p, mesas m where p.fk_mesa = m.id and p.estatus = ?', 'En cobro')    
+    const payment = await connection.query('select p.id, m.mesa, p.totalpedido from pedidolocal p, mesas m where p.fk_mesa = m.id and p.estatus = ?', 'En cobro')
     res.render('system/admin/admin.payment.hbs', {
         payment
     })
@@ -22,7 +22,14 @@ controller.SavePayment = async (req, res) => {
     const { id } = req.params
     let today = new Date()
     const fecha = today.toLocaleDateString("en-US")
+
     try {
+        const productos = await connection.query('select fk_producto, cantidad from productospedidolocal where fk_pedidolocal = ?', [id])
+        productos.forEach(async productopl => {
+            const cantidadalmacen = await connection.query('select existencias from almacen where fk_producto = ?', [productopl.fk_producto])
+            const operacion = parseInt(cantidadalmacen[0].existencias) - parseInt(productopl.cantidad)
+            await connection.query('update almacen set existencias = ? where fk_producto = ?', [operacion, productopl.fk_producto])
+        });
         await connection.query('insert into ventalocal values (null,?,?)', [fecha, id])
         await connection.query(`update pedidolocal set estatus = 'Cobrado' where id = ?`, [id])
         req.flash("success_msg", "Venta hecha correctamente.")
