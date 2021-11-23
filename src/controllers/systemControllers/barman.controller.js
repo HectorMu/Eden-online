a5c4d4c534ba05566110499168c915c1e2d04472
Juan Luis <jl.noriega2000@gmail.com>
2021-11-23 00:36:26 -0600

    Se corrigieron algunos dashboard y se dio cuello a la campana

--- src/controllers/systemControllers/barman.controller.js
+++ src/controllers/systemControllers/barman.controller.js
@@ -2,9 +2,10 @@ const connection = require('../../database')
 const controller = {}
 
 controller.renderBarmanDashboard = async (req, res)=>{
-    const products = await connection.query(`select COUNT(pl.fk_producto) as products from productospedidolocal pl, productos p where p.fk_categoria = 1 && pl.estatus = 'Preparacion' && pl.fk_producto = p.id;`)
+    const productslocals = await connection.query(`select COUNT(*) as products from productospedidolocal pl, productos p where p.fk_categoria = 1 && pl.estatus = 'Preparacion' && pl.fk_producto = p.id;`)
+    const productsline = await connection.query(`select COUNT(*) as products from productospedidolinea pl, productos p where p.fk_categoria = 1 && pl.estatus = 'Preparacion' && pl.fk_producto = p.id;`)
     res.render('system/barman/dashboard', {
-        Products: products[0].products
+        Productslocals: productslocals[0].products, Productsline: productsline[0].products
     })
 }
 controller.renderBarmanOrders=(req, res)=>{
